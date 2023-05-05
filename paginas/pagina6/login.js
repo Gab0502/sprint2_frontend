04144a84466b75aedbdedea8efe3d2c6511e787e
GAbriel <gabmt2210@gmail.com>
2023-05-05 10:58:02 -0300

    update

--- paginas/pagina6/login.js
+++ paginas/pagina6/login.js
@@ -1,14 +1,15 @@
-let username = document.getElementById("username").value;
-let senha = document.getElementById("senha").value;
+
 
 function login(){
+    let username = document.getElementById("username").value;
+    let senha = document.getElementById("senha").value;
     if (username == "adm") {
         document.write ("Seja bem vindo á página de Administrador")
         window.location.href = "/paginas/paginaadm/paginaadm.html"
     }
     else if (username =="morador"){
         document.write ("Seja bem vindo á página de morador")
-        window.location.href = "/paginas/morador/morador.html"
+        window.location.href = "/paginas/paginamorador/morador.html"
     }
     else{
         alert("login ou senha incorretos")
