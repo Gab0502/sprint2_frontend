31ee3c7c3584523b831fe5c1cb1ba757743706fd
felipe quinalha <felipe.quinalha2013@gmail.com>
2023-05-21 03:57:37 -0300

    js

--- home/mode.js
+++ home/mode.js
@@ -15,3 +15,42 @@ function nextImage(){
   }
   document.getElementById("radio"+count).checked = true;
 }
+function dkm(darkmode){
+  document.body.style.background = darkmode
+}
+
+
+function login(){
+  let username = document.getElementById("username").value;
+  let senha = document.getElementById("senha").value;
+  if (username == "adm") {
+      document.write ("Seja bem vindo á página de Administrador")
+      window.location.href = "/paginas/paginaadm/paginaadm.html"
+  }
+  else if (username =="morador"){
+      document.write ("Seja bem vindo á página de morador")
+      window.location.href = "/paginas/paginamorador/morador.html"
+  }
+  else{
+      alert("login ou senha incorretos")
+  }
+}
+
+function verificarua() {
+  const ruaoutput = document.getElementById("ruaoutput");
+  const rua = document.getElementById("rua").value;
+  
+  if (rua == "Rua Eng. Silva Braga") {
+    ruaoutput.innerHTML = "A rua está com certa anomalia, fique atento para mais atualizações.";
+  }
+  else if (rua == "Rua Marques de Lages"){
+    ruaoutput.innerHTML = "A rua apresenta valores normais, aproveite seu dia!"
+  }
+  else if(rua == "Rua dos Operarios" ){
+    ruaoutput.innerHTML = "A rua esta em situação grave, por favor proteja seus bens e seus familiares"
+  }
+  else{
+    ruaoutput.innerHTML = "Desculpe ainda não trabalhamos com essa rua"
+  }
+}
+
